0f70b8a1a54e20a5ced6a06a8205c979159179f0
Clervens Volcy <clervens.volcy@gmail.com>
2014-12-28 13:03:52 -0500

    MOD : customize fast theme

--- wp-content/themes/fastr/header.php
+++ wp-content/themes/fastr/header.php
@@ -14,6 +14,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 
 <?php wp_head(); ?>
 </head>
