f2d9177d668a14d22cbe3c4ac6452efd3a9675ab
EQUISOFT\CVolcy <clervens.volcy@gmail.com>
2017-03-03 14:35:16 -0500

    use es6 syntax since we're using node v4+

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var http = require('http');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var fs = require('fs');
-var md = require('markdown-it')({
-  html:true
-});
+const http 	= require('http'),
+	path 	= require('path'),
+	express = require('express'),
+	bodyParser 	= require('body-parser'),
+	morgan 	= require('morgan'),
+	fs 		= require('fs'),
+	md 		= require('markdown-it')({
+  				html:true
+			});
 
 var app = express();
 var server = http.createServer(app);
@@ -18,23 +18,26 @@ app.use(morgan('common'));
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.set('view engine', 'pug');
 
-app.get("/", function(req, res, next) {
+app.get("/", (req, res, next) => {
 	res.render('index');
 });
 
-app.get("/project/:project", function(req, res, next) {
-	var project = req.params.project.replace(/[^\-0-9a-zÀ-ÿ]/gi, '_').toLowerCase();
-	var ppath = path.resolve(__dirname, 'projects', project + '.md');
-	fs.readFile(ppath, 'utf8', function (err,data) {
+app.get("/project/:project", (req, res, next) => {
+	let project = req.params.project.replace(/[^\-0-9a-zÀ-ÿ]/gi, '_').toLowerCase();
+	let ppath = path.resolve(__dirname, 'projects', project + '.md');
+
+	console.log(`Trying to render project "${project}" from path "${ppath}"`);
+
+	fs.readFile(ppath, 'utf8', (err,data) => {
 		if (err) {
 			return res.redirect('/');
 		}
-		var projectTemplate = md.render(data);
+		let projectTemplate = md.render(data);
 		res.render('project', { project: projectTemplate });
 	});
 });
 
-server.listen(process.env.PORT || 4000, process.env.IP || "0.0.0.0", function(){
-	var addr = server.address();
+server.listen(process.env.PORT || 4000, process.env.IP || "0.0.0.0", () => {
+	let addr = server.address();
 	console.log("Server listening at", addr.address + ":" + addr.port);
 });
