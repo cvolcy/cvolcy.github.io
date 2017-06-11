78b752b613872aef40fed699824bab84701363ca
Clervens Volcy <clervens.volcy@gmail.com>
2017-06-11 16:56:29 -0400

    Enable build on deploy

--- content/scripts/site.js
+++ content/scripts/site.js
@@ -1,6 +1,7 @@
-// nothing 
+// nothing
 $(document).ready(function() {
   $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
   });
-}); 
+  $('.button-collapse').sideNav({draggable: true});
+});
