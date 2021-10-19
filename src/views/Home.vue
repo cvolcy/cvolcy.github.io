50d0b54e6fad0ec32b83e219e1db61ecb816f607
Clervens Volcy <clervens.volcy@gmail.com>
2021-10-18 22:07:36 -0400

    remove unused component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,19 +14,12 @@
         </p>
       </div>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue';
+import { Vue } from 'vue-class-component';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
 export default class Home extends Vue {}
 </script>
 
