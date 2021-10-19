a4c8bbfacb6c7356325b666d43cf071138b3d760
Clervens Volcy <clervens.volcy@gmail.com>
2021-10-18 22:06:40 -0400

    navbar update

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="header">
-    <div v-if="active" class="nav-padding" />
-    <div :class="['nav', { sticky: active }]">
-      <div class="logo"></div>
-      <div class="menu">
-        <ul>
-          <li>
-            <router-link to="/">Home</router-link>
-          </li>
-          <li>
-            <router-link to="/projects">Projects</router-link>
-          </li>
-          <li>
-            <router-link to="/about">About</router-link>
-          </li>
-        </ul>
+    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a role="button" aria-label="menu" aria-expanded="false"
+          :class="['navbar-burger', {'is-active': menuIsActive }]"
+          @click="toggleMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28">
+        </a>
       </div>
-      <div class="socials"></div>
-    </div>
+      <div :class="['navbar-menu', {'is-active': menuIsActive }]">
+        <div class="navbar-items">
+          <div class="navbar-item">
+            <router-link to="/">Home</router-link>
+          </div>
+          <div class="navbar-item">
+            <router-link to="/projects">Projects</router-link>
+          </div>
+          <div class="navbar-item">
+            <router-link to="/about">About</router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 <script lang="ts">
@@ -25,60 +35,55 @@ import { Options, Vue } from 'vue-class-component';
 
 @Options({})
 export default class Header extends Vue {
-  active = false;
+  menuIsActive = false;
 
-  mounted(): void {
-    window.document.onscroll = () => {
-      if (window.scrollY > this.$el.offsetTop) {
-        this.active = true;
-      } else {
-        this.active = false;
-      }
-    };
+  toggleMenu(): void {
+    this.menuIsActive = !this.menuIsActive;
   }
 }
 </script>
 <style lang="scss" scoped>
-.nav {
-  padding: 30px;
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
-  height: 30px;
+@import 'bulma/sass/components/navbar';
+
+nav.navbar {
+  background-color: initial;
   border-bottom: solid 1px $border-color;
 
-  &.sticky {
-    z-index: 9999;
-    position: fixed;
-    top: 0;
-    right: 0;
-    left: 0;
-    background-color: rgba($bg-color, 0.9);
+  .navbar-burger {
+    margin-left: 0;
   }
 
-  ul {
-    list-style: none;
-    text-align: center;
-    position: relative;
+  .navbar-menu {
+    justify-content: center;
 
-    li {
-      display: inline-block;
-      position: relative;
-      padding: 0 45px 0 0;
+    .navbar-items {
+      display: flex;
+
+      @include until(1005px) {
+        flex-direction: column;
+      }
+
+      .navbar-item {
+        padding: 0 20px 0;
+
+        &:hover, :focus {
+          background-color: transparent;
+        }
+
+        @include until(1005px) {
+          margin: 10px 0;
+        }
+
+        a {
+          font-weight: bold;
+          text-transform: uppercase;
+
+          &.router-link-exact-active {
+            color: $link-active-color;
+          }
+        }
+      }
     }
   }
-
-  a {
-    font-weight: bold;
-    text-transform: uppercase;
-
-    &.router-link-exact-active {
-      color: $link-active-color;
-    }
-  }
-}
-
-.nav-padding {
-  height: 90px;
 }
 </style>
