9da093dae13e195b1dfefdb5ab6b05b60a462141
Clervens Volcy <clervens.volcy@gmail.com>
2021-11-15 14:36:25 -0500

    gh-pages

---
+++ precache-manifest.7f0412ec972b2e01757747e79f394fb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b6b1eede11f3ba8dca",
+    "url": "/css/app.8b45638c.css"
+  },
+  {
+    "revision": "632b36cf16b4aa02ce56ce05b640d05e",
+    "url": "/img/avatar.632b36cf.png"
+  },
+  {
+    "revision": "960726dd278c68f5d2d48c17c08bb50f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "734564e45dd5e9b1e318",
+    "url": "/js/about.8f70e0e7.js"
+  },
+  {
+    "revision": "50b6b1eede11f3ba8dca",
+    "url": "/js/app.21fe6424.js"
+  },
+  {
+    "revision": "6021656e6a5d16aa0dc4",
+    "url": "/js/chunk-vendors.6ebf7994.js"
+  },
+  {
+    "revision": "da459592e2ee282112f63ec5b176785c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
