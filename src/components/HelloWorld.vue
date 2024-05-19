faf454df7e6a0314480e0db6963f49cc110f8e1c
Clervens Volcy <clervens.volcy@gmail.com>
2024-05-19 19:09:14 -0400

    added home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,18 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="min-h-screen flex items-center">
+    <div class="flex flex-wrap">
+      <div class="flex flex-1 basis-80">
+        <img class="rounded-full w-80 h-80" src="/avatar.png" alt="avatar" />
+      </div>
+      <div class="flex flex-1 flex-col text-left justify-around basis-80">
+        <h1 class="text-8xl">Hello</h1>
+        <h2 class="text-4xl">I'm Clervens Volcy</h2>
+        <p>
+          a programmer from Montréal<br />
+          I'm Software Engineer bachelor with a keen interest in solving problems, Web technologies, machine learning
+          and artificial intelligence.
+        </p>
+      </div>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
