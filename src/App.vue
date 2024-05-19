4c374f646b9b35a360f2f5056d9a0440a4ca0934
Clervens Volcy <clervens.volcy@gmail.com>
2024-05-19 18:36:11 -0400

    intialize vue 3 w/ vite

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
-<template>
-  <Header />
-  <div class="container">
-    <router-view/>
-  </div>
-  <Footer />
-</template>
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
-
-@Options({
-  components: { Footer, Header },
-})
-export default class About extends Vue {
-}
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
-<style lang="scss">
-body {
-  a {
-    text-decoration: none;
-    transition: color 400ms, background-color 400ms;
 
-    &:hover {
-      color: $link-hover-color;
-      text-decoration: revert;
-    }
-  }
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-
-  > .container {
-    padding-top: 80px;
-    min-height: calc(100vh - 103px);
-
-    @include mobile {
-      margin-right: 10px;
-      margin-left: 10px;
-    }
-  }
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import { createApp } from 'vue';
-import './assets/main.scss';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).mount('#app')
