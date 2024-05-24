6d63d2a1d5677d1f51b6f0eaf3d40b0df66247be
Clervens Volcy <clervens.volcy@gmail.com>
2024-05-24 13:36:15 -0400

    Added Section component

---
+++ src/components/Section.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+    id: string
+}>();
+</script>
+<template>
+    <div class="flex flex-col my-4">
+        <span :id class="-mt-16 mb-16"/>
+        <p v-if="$slots.title" class="text-4xl mb-4 uppercase">
+            <slot name="title"></slot>
+        </p>
+        <slot></slot>
+    </div>
+</template>
