1ac778771a9f0ad06548fc1919cecac0680a03d4
Clervens Volcy <clervens.volcy@gmail.com>
2024-05-24 13:19:39 -0400

    Refactor : created ThemeSwitcher

---
+++ src/components/navbar/ThemeSwitcher.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useColorMode } from '@vueuse/core'
+import { Button } from '@/components/ui/button'
+import { SunIcon, MoonIcon } from 'lucide-vue-next'
+
+const mode = useColorMode();
+const icon = computed(() => mode.value == 'dark' ? SunIcon : MoonIcon);
+</script>
+<template>
+    <Button
+        variant="ghost"
+        :title="`${ mode } mode`"
+        @click="mode = mode === 'dark' ? 'light' : 'dark'"
+    >
+        <component :is="icon" />
+    </Button>
+</template>
